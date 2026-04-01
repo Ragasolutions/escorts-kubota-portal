76fe6f2b622b256a2eadb092e5851155c5227da4
Aman Saini <amansaini2537@gmail.com>
2026-04-01 15:29:44 +0530

    update

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 })
 app.use(morgan('dev'));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://escorts-kubota-portal.vercel.app',
   credentials: true,
 }))
 app.use(express.json());
